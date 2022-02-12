4146e867ff47ba56511bcef5ae38b73950fa087d
taaag51 <inat0510+git@gmail.com>
2022-02-13 01:36:24 +0900

    commit

--- list2/go.mod
+++ list2/go.mod
@@ -2,4 +2,4 @@ module list2
 
 go 1.17
 
-require github.com/taaag51/golang-handson/hello v0.0.0-20220208151337-f2e2894bc074
+require github.com/taaag51/golang-handson/hello v0.0.0-20220212153649-54ca26431859
---
+++ list2/list2-10.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/taaag51/golang-handson/hello"
+)
+
+func main() {
+	x := hello.Input("type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Print("1から" + x + "の合計は、")
+	} else {
+		return
+	}
+	t := 0
+	c := 1
+	for c <= n {
+		t += c
+		c++
+	}
+	fmt.Println(t, "です。")
+}

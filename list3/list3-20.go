7f7eeb53013cde285bb94ef5518f4d349ac31630
taaag51 <inat0510+git@gmail.com>
2022-02-18 09:41:37 +0900

    2022/02/18の朝の分

---
+++ list3/list3-20.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Generalis  all type data.
+type General interface{}
+
+// GData is holding General value.
+type GData interface {
+	Set(nm string, g General)
+	Print()
+}
+
+// GData is structure.
+type GDataImpl struct {
+	Name string
+	Data General
+}
+
+// Set is GDataImpl method.
+func (gd *GDataImpl) Set(nm string, g General) {
+	gd.Name = nm
+	gd.Data = g
+}
+
+// Print is GDataImpl method.
+func (gd *GDataImpl) Print() {
+	fmt.Printf("<<%s>> ", gd.Name)
+	fmt.Println(gd.Data)
+
+}
+
+func main() {
+	var data = []GDataImpl{}
+	data = append(data, GDataImpl{"Taro", 123})
+	data = append(data, GDataImpl{"Hanako", "hello!"})
+	data = append(data, GDataImpl{"Sachiko", []int{123, 456, 789}})
+	for _, ob := range data {
+		ob.Print()
+
+	}
+}
